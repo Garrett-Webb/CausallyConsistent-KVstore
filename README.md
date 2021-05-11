895aad29510d9a86b516f2982e15d932a8c183bf
Garrett Webb <garrettwebb97@gmail.com>
2021-05-11 11:50:36 -0700

    initializing repo, downloaded files, copied Assignment2 server.py

---
+++ README.md
@@ -0,0 +1,18 @@
+# CSE138_Assignment3
+Private repository for CSE138 - Distributed Systems - Assignment 3
+
+## CruzIDs
+* `gswebb`
+* `kahsieh`
+* `raarora`
+
+## Citations:
+* 
+
+## Team Contributions:
+* Garrett: 
+* Rahul: 
+* Kai: 
+
+## Acknowledgements:
+* 
---
+++ server.py
@@ -0,0 +1,174 @@
+# server.py
+
+# Assignment 2
+# CruzIDs
+# Garrett Webb: gswebb
+# Kai Hsieh: kahsieh
+# Rahul Arora: raarora
+
+#CSE138 Kuper Spring 2021
+
+#acknowledgements
+#https://stackabuse.com/serving-files-with-pythons-simplehttpserver-module/
+#https://docs.python.org/3/library/http.server.html
+#https://stackoverflow.com/questions/31371166/reading-json-from-simplehttpserver-post-data
+#https://realpython.com/python-requests/#headers
+
+import requests
+import sys
+import http.server
+import socketserver
+import json
+from sys import argv
+import os
+kvstore = {}
+main_flag = False
+faddr = ""
+
+class requestHandler(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self, response_code):
+        self.send_response(response_code)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+    def do_GET(self):
+        if (main_flag == False):
+            try:
+                r = requests.get('http://' + faddr + str(self.path), headers=self.headers)
+                self._set_headers(r.status_code)
+                self.wfile.write(r.content)
+            except:
+                self._set_headers(response_code=503)
+                response = bytes(json.dumps({"error":"Main instance is down","message":"Error in GET"}), 'utf-8')
+                self.wfile.write(response)
+        
+        else:
+            if "/key-value-store/" in str(self.path):
+                keystr = str(self.path).split("/key-value-store/",1)[1]
+                if(len(keystr) > 0 and len(keystr) < 50):
+                    if keystr in kvstore:
+                        self._set_headers(response_code=200)
+                        response = bytes(json.dumps({"doesExist" : True, "message" : "Retrieved successfully", "value" : kvstore[keystr]}), 'utf-8')
+                    else:
+                        self._set_headers(response_code=404)
+                        response = bytes(json.dumps({"doesExist" : False, "error" : "Key does not exist", "message" : "Error in GET"}), 'utf-8')
+                elif (len(keystr) > 50):
+                    self._set_headers(response_code=400)
+                    response = bytes(json.dumps({'error' : "Key is too long", 'message' : "Error in GET"}), 'utf-8')
+                elif(len(keystr) == 0):
+                    self._set_headers(response_code=400)
+                    response = bytes(json.dumps({'error' : "Key not specified", 'message' : "Error in GET"}), 'utf-8')
+                self.wfile.write(response)
+            else:
+                #default 500 code to clean up loose ends
+                self._set_headers(response_code=500)
+        return
+
+    def do_PUT(self):
+        if(main_flag == False):
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            data_send = json.loads(self.data_string)
+            try:
+                r = requests.put('http://' + faddr + str(self.path), json=data_send, headers=self.headers)
+                self._set_headers(r.status_code)
+                self.wfile.write(r.content)
+            except:
+                self._set_headers(response_code=503)
+                response = bytes(json.dumps({"error":"Main instance is down","message":"Error in PUT"}), 'utf-8')
+                self.wfile.write(response)
+        else:
+            if "/key-value-store/" in str(self.path):
+                keystr = str(self.path).split("/key-value-store/",1)[1]
+                if(len(keystr) > 0 and len(keystr) < 50):
+                    self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+                    data = json.loads(self.data_string)
+                    if "value" not in data:
+                        self._set_headers(response_code=400)
+                        response = bytes(json.dumps({'error' : "Value is missing", 'message' : "Error in PUT"}), 'utf-8')
+                    elif keystr in kvstore:
+                        kvstore[keystr] = data["value"]
+                        self._set_headers(response_code=200)
+                        response = bytes(json.dumps({'message' : "Updated successfully", 'replaced' :True}), 'utf-8')
+                    else:
+                        kvstore[keystr] = data["value"]
+                        self._set_headers(response_code=201)
+                        response = bytes(json.dumps({'message' : "Added successfully", 'replaced' :False}), 'utf-8')
+                elif (len(keystr) > 50):
+                    self._set_headers(response_code=400)
+                    response = bytes(json.dumps({'error' : "Key is too long", 'message' : "Error in PUT"}), 'utf-8')
+                
+                self.wfile.write(response)
+            else:
+                self._set_headers(response_code=500)
+        return
+    
+    def do_DELETE(self):
+        if (main_flag == False):
+            try:
+                r = requests.delete('http://' + faddr + str(self.path), headers=self.headers)
+                self._set_headers(r.status_code)
+                self.wfile.write(r.content)
+            except:
+                self._set_headers(response_code=503)
+                response = bytes(json.dumps({"error":"Main instance is down","message":"Error in DELETE"}), 'utf-8')
+                self.wfile.write(response)
+        else:
+            if "/key-value-store/" in str(self.path):
+                keystr = str(self.path).split("/key-value-store/",1)[1]
+                if(len(keystr) > 0 and len(keystr) < 50):
+                    if keystr in kvstore:
+                        del kvstore[keystr]
+                        self._set_headers(response_code=200)
+                        response = bytes(json.dumps({"doesExist" : True, "message" : "Deleted successfully"}), 'utf-8')
+                    else:
+                        self._set_headers(response_code=404)
+                        response = bytes(json.dumps({"doesExist" : False, "error" : "Key does not exist", "message" : "Error in DELETE"}), 'utf-8')
+                elif (len(keystr) > 50):
+                    self._set_headers(response_code=400)
+                    response = bytes(json.dumps({'error' : "Key is too long", 'message' : "Error in DELETE"}), 'utf-8')
+                elif(len(keystr) == 0):
+                    self._set_headers(response_code=400)
+                    response = bytes(json.dumps({'error' : "Key not specified", 'message' : "Error in DELETE"}), 'utf-8')
+                self.wfile.write(response)
+            else:
+                #default 500 code to clean up loose ends
+                self._set_headers(response_code=500)
+        return
+
+def run(server_class=http.server.HTTPServer, handler_class=requestHandler, addr='', port=8085):
+    # this function initializes and runs the server on the class defined above
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting HTTP server on {addr}:{port}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+if __name__ == '__main__':
+    d_ip = ''
+    d_port = 8085
+    try:
+        faddr = os.environ['FORWARDING_ADDRESS']
+        if len(faddr) > 0:
+            print("faddr: " + str(faddr))
+            d_port = str(faddr).split(":",1)[1]
+            print("d_port: " + str(d_port))
+            d_port = int(d_port)
+    except:
+        print("main instance")
+        main_flag = True
+        
+    print(main_flag)
+    x = 0
+    for arg in argv:
+        print("arg" + str(x) + ": " + str(argv[x]))
+        x = x+1
+
+    if len(argv) == 2:
+        #call the run function with custom port
+        run(port=int(argv[1]))
+    else:
+        #call the run function with default port 8085
+        run()
